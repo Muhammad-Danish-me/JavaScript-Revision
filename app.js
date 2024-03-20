3d17cffd5b8d74e4cad3edd523286333b2a04a02
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-20 12:12:33 +0500

    JavaScript Revision

---
+++ app.js
@@ -0,0 +1,60 @@
+// chapter-1 alert
+// An alert is a box that pops up to give the user a message.
+
+// alert("Hello world")
+
+// chapter-2 Variables for strings
+var name = "Danish";
+var nationality = "Pakistan";
+
+var details = name + " \n" + nationality;
+// alert (details)
+
+// declared variable in one statement then assign value
+var fullName;
+
+fullName = "Muhammad Danish";
+
+// variable name must be relate the value
+
+// chapter-3 Variables for Numbers
+
+// A string isn't the only thing you can assign to a variable. You can also assign a number
+
+var num = 20;
+var num2 = 30;
+var total = num + num2;
+console.log(total);
+
+var originalNum = 50;
+var numToBeAdded = originalNum + 90;
+console.log(numToBeAdded);
+
+// Chapter 4 Variable Names Legal and Illegal
+// You've already learned three rules about naming a variable: You can't enclose it in
+// quotation marks. The name can't be a number or start with a number. It can't be any of
+// JavaScript's keywords—the special words that act as programming instructions, like alert
+// and var
+
+// A variable name can't contain any spaces.
+
+// legal name
+// camelCase
+// snake_case
+
+// chapter 5 Math expressions:
+//           Familiar operators
+
+var add = 2 + 2;
+var sub = 2 - 2;
+var multiply = 2 * 2;
+var devide = 2 / 2;
+var modulas = 2 % 2;
+
+
+// chapter 6 Math expressions:
+//           Unfamiliar operators
+
+num = 1
+num
+
