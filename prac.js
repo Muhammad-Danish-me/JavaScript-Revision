a66596fcd105bb438147dbc065b93259ad708a21
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-03-31 01:52:35 +0500

    Revision

---
+++ prac.js
@@ -0,0 +1,37 @@
+// var userName = prompt("Enter your username")
+// if (userName == "male"){
+//     alert("you are male")
+// }else if (userName == "female"){
+//     alert("you are female")
+// }else{
+//     alert("invalid input")
+// }
+
+
+// var education = prompt("Enter your education");
+
+// if (education !== "matric"){
+//     alert("eligible")
+// }else{
+//     alert("not eligible")
+// }
+
+
+// var age = prompt("Enter Your Age! Must be 18 or above otherwise not eligble")
+// if (age >= 18){
+//  alert("eligible")
+// }else  {
+//     alert("Not Eligible")
+// }
+
+
+var friesLekarAo = prompt("fries kidr se laye ho")
+if (friesLekarAo === "khubaib"){
+    alert("Phyli Dafa koi acha kam kya")
+}else if (friesLekarAo === "waseem"){
+    alert("is se nhi lani thi. Chalo Khair hai")
+}else if (friesLekarAo === "imran"){
+    alert("is se nhi lani thi chalo khair hai")
+}else{
+    alert("wapis krwao mene iski nhi khani")
+}
