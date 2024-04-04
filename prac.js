9e6f56a54cb11521d1a6e4eebf7b12fef4ecb408
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-04-05 01:09:18 +0500

    Update prac.js

--- prac.js
+++ prac.js
@@ -184,12 +184,13 @@
 
 // min & max
 
-var arr = [2,5,6,8,3,1,9,7]
-var min = arr[0]
+// var arr = [2,5,6,8,3,1,9,7]
+// var min = arr[0]
 
-for(var i = 0; i < arr.length; i++){
-    if (arr[i] < min){
-        min = arr[i]
-    }
-}
-console.log(min)
+// for(var i = 0; i < arr.length; i++){
+//     if (arr[i] < min){
+//         min = arr[i]
+//     }
+// }
+// console.log(min)
+
