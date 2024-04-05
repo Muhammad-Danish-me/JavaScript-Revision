f72fd2f96244e574b36858cd8adfe13f789699c1
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-04-06 00:14:00 +0500

    Added new.js and new.html

---
+++ new.js
@@ -0,0 +1,16 @@
+var text = "My Name is Muhammad Danish"
+
+for(var i = 0; i < text.length; i++){
+    console.log(text.slice(i, i + 6) === "Danish")
+}
+
+
+var myText = "Oligodon, commonly known as the kukri snakes, is a genus of colubrid snakes that was first described by Austrian zoologist Leopold Fitzinger in 1826."
+
+var user = prompt("Enter your word")
+
+for(var i = 0; i < myText.length; i++){
+    if(myText.slice(i, i + user.length) === user){
+        console.log("found")
+    }
+}
