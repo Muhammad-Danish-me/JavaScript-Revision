256f40e9c68cfe38855fb40480ea850f727a841c
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-04-05 23:32:10 +0500

    Update prac.js

--- prac.js
+++ prac.js
@@ -203,3 +203,12 @@
 //    }
 
 // }
+
+var arr = [["Danish" , "Ghunio"],
+          ["Hammad", "Khan"]]
+
+for (var i = 0; i < arr.length; i++){
+    for(var j = 0; j < arr[i].length; j++){
+        console.log(arr[i][j]);
+    }
+}
