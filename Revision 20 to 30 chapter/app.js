0c9bc0c98d79ecefb311725d7da67d02a36b0acb
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-04-07 00:35:03 +0500

    Practice

---
+++ Revision 20 to 30 chapter/app.js
@@ -0,0 +1,15 @@
+// changing case
+
+var fullName = "Muhammad Danish"
+
+console.log(fullName.toUpperCase())
+
+var fullName = "Muhammad Danish"
+
+console.log(fullName.toLowerCase())
+
+
+
+var text = "Hello world"
+
+console.log(text.length)
