0c221dccb6f54ab25f38a67fc2ba54457d4c5fc3
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-04-18 16:11:08 +0500

    if statement revision
    practice

---
+++ if statement/app.js
@@ -0,0 +1,10 @@
+var skills = prompt("Enter your skills");
+if (skills === "html") {
+  var exp = +prompt("enter Your Experience");
+  if (exp > 2) {
+    var city = prompt("Enter your city");
+  } else alert("Not Hired");
+  if (city === "karachi") {
+    alert("hired");
+  } else alert("Not Hired");
+} else alert("not hired");
